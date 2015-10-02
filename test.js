64f6262b5087f8c89cd5f12ee0a5a4c264701c6a
Matti Schneider <matti.schneider@data.gouv.fr>
2015-10-02 18:11:02 +0200

    refactor tests

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import expect from 'expect.js';
 import { parsePaje } from './findPaje';
 
 
-describe('non-paje simulations', () => {
+describe('non simulations', () => {
 	const RECORD = [ 441,
 		'01/07/2014',
 		'10:02:55',
@@ -17,8 +17,28 @@ describe('non-paje simulations', () => {
 	});
 });
 
-describe('paje simulations', () => {
-	describe('starts', () => {
+describe('simulations', () => {
+	const RECORD = [ 441,
+		'01/07/2014',
+		'10:02:55',
+		'81.50.X.X',
+		'DOSWEB',
+		'PAJALLWEB',
+		'9462cdb3cb9c194bdfe925c5628cc9222bc846950888f7d5db2e1832fd54aff6' ];
+
+	it('should return an object', () => {
+		expect(parsePaje(RECORD)).to.be.an('object');
+	});
+
+	it('should give the simulation date', () => {
+		expect(parsePaje(RECORD).date).to.eql(new Date(2014, 6, 1, 10, 2, 55));
+	});
+
+	it('should give the user identifier', () => {
+		expect(parsePaje(RECORD).user).to.be('9462cdb3cb9c194bdfe925c5628cc9222bc846950888f7d5db2e1832fd54aff6');
+	});
+
+	describe('paje start', () => {
 		const RECORD = [ 441,
 			'01/07/2014',
 			'10:02:55',
@@ -27,29 +47,16 @@ describe('paje simulations', () => {
 			'PAJALLWEB',
 			'9462cdb3cb9c194bdfe925c5628cc9222bc846950888f7d5db2e1832fd54aff6' ];
 
-		let subject = parsePaje(RECORD);
-
-		it('should return an object', () => {
-			expect(subject).to.be.an('object');
-		});
-
 		it('should give the simulation type', () => {
-			expect(subject.type).to.be('paje');
-		});
-
-		it('should give the simulation date', () => {
-			expect(subject.date).to.eql(new Date(2014, 6, 1, 10, 2, 55));
-		});
-
-		it('should give the user identifier', () => {
-			expect(subject.user).to.be('9462cdb3cb9c194bdfe925c5628cc9222bc846950888f7d5db2e1832fd54aff6');
+			expect(parsePaje(RECORD).type).to.be('paje');
 		});
 
 		it('should give the simulation status', () => {
-			expect(subject.status).to.be('start');
+			expect(parsePaje(RECORD).status).to.be('start');
 		});
-	})
-	describe('ends', () => {
+	});
+
+	describe('paje ends', () => {
 		const RECORD = [ 441,
 			'01/07/2014',
 			'10:02:55',
@@ -58,26 +65,12 @@ describe('paje simulations', () => {
 			'PAJRESUWEB',
 			'9462cdb3cb9c194bdfe925c5628cc9222bc846950888f7d5db2e1832fd54aff6' ];
 
-		let subject = parsePaje(RECORD);
-
-		it('should return an object', () => {
-			expect(subject).to.be.an('object');
-		});
-
 		it('should give the simulation type', () => {
-			expect(subject.type).to.be('paje');
-		});
-
-		it('should give the simulation date', () => {
-			expect(subject.date).to.eql(new Date(2014, 6, 1, 10, 2, 55));
-		});
-
-		it('should give the user identifier', () => {
-			expect(subject.user).to.be('9462cdb3cb9c194bdfe925c5628cc9222bc846950888f7d5db2e1832fd54aff6');
+			expect(parsePaje(RECORD).type).to.be('paje');
 		});
 
 		it('should give the simulation status', () => {
-			expect(subject.status).to.be('end');
+			expect(parsePaje(RECORD).status).to.be('end');
 		});
 	});
 });
